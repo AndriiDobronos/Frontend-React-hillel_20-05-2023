8b47768bea0ba844b5391f6681d71ffff13476fb
Andrii Dobronos <andriidobronos@gmail.com>
2023-06-01 12:52:23 +0300

    added component Form.jsx

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
 import './App.css';
 //import ChangeProfile from "./components/ChangeProfile.jsx";
-import EditPost from "./components/EditPost";
+//import EditPost from "./components/EditPost";
 import React from "react";
+import Form from "./components/Form";
 
 export default class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {posts:[]};
     }
-
+/* *************************** for EditPost.jsx *********************************
     componentDidMount() {
         fetch(`https://jsonplaceholder.typicode.com/posts`)
             .then((response) => response.json())
-//            .then((json) => this.setState({posts:[...json].map(content=> content.id)})
             .then((json) => this.setState({posts:[...json].map(content=> ({id:content.id}))})
 
             )
-    }
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if (prevState.posts !== this.state.posts ) {
-//            document.getElementById("notification2").textContent = "Элемент обновился";
-        }
-    }
+    }  */
+
 
     render () {
         return <div className="App">
 
             {/*<ChangeProfile />*/}
 
-            {this.state.posts.map(content => {
+            {/*this.state.posts.map(content => {
                 return <EditPost key={content.id} id={content.id}/>}
-            )}
+            )*/}
+
+            <Form />
 
         </div>
     }
